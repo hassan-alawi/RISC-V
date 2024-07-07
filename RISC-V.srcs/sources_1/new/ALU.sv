d4708fa27cf4365ddcc7ca5dbc3c0fe9992643d2
hassan-alawi <hassanalalawi2003@gmail.com>
2024-07-07 13:56:42 -0400

    ALU written

--- RISC-V.srcs/sources_1/new/ALU.sv
+++ RISC-V.srcs/sources_1/new/ALU.sv
@@ -21,5 +21,44 @@
 
 
 module ALU(
+    input logic [31:0] rs1, rs2,
+    input logic [3:0] ALU_control,
+    output logic [31:0] result,
+    output logic zero
     );
+    
+    // ALU_control meaning
+    // 0010: add
+    // 0110: sub
+    // 0000: and
+    // 0001: or
+    
+    assign zero = result == '0;
+    
+    always_comb begin: ALU_LUT
+    
+        case(ALU_control)
+        
+        4'b0010: begin
+            result = rs1 + rs2;
+        end
+        
+        4'b0110: begin
+            result = rs1 - rs2;
+        end
+        
+        4'b0000: begin
+            result = rs1 & rs2;
+        end
+        
+        4'b0001: begin
+            result = rs1 | rs2;
+        end
+        
+        default: result = '1;
+        
+        endcase
+        
+    end
+    
 endmodule
