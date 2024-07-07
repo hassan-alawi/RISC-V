a60b9032948bb3596c15485f31e8bac0efd90e84
hassan-alalawi <hassanalalawi2003@gmail.com>
2024-07-07 13:50:52 -0400

    all LUTs written

---
+++ RISC-V.srcs/sources_1/new/immediate_generator.sv
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/06/2024 08:22:48 PM
+// Design Name: 
+// Module Name: imeddiate_generator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module immediate_generator(
+    input logic [31:0] instruction,
+    output logic [31:0] sign_extended_immediate
+    );
+    
+    logic [6:0] opcode;
+    assign opcode = instruction[6:0];
+    
+    always_comb begin: IMMEDIATE_LUT
+    
+        case(opcode)
+        
+        7'b0110011: begin //R-Type Instructions
+            sign_extended_immediate = '0;
+        end
+        
+        7'b0010011: begin //I-Type instruction
+            sign_extended_immediate = instruction[31] ? {'1,instruction[30:20]} : {'0,instruction[30:20]};
+        end
+        
+        7'b0000011: begin //lw instruction
+            sign_extended_immediate = instruction[31] ? {'1,instruction[30:20]} : {'0,instruction[30:20]};
+        end
+        
+        7'b0100011: begin //sw instruction
+            sign_extended_immediate = instruction[31] ? {'1,instruction[30:25], instruction[11:7]} : {'0,instruction[30:25], instruction[11:7]};
+        end
+        
+        7'b1100011: begin //beq and SB-Type instruction 
+            sign_extended_immediate = instruction[31] ? {'1, instruction[7], instruction[30:25], instruction[11:8], 1'b0} : {'0, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+        end
+        
+        7'b1101111: begin //jal and UJ-Type instruction
+            sign_extended_immediate = instruction[31] ? {'1, instruction[19:12], instruction[20], instruction[30:21], 1'b0} : {'0, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+        end
+        
+        7'b0101111: begin //lui and U-Type instruction
+            sign_extended_immediate = {instruction[31:12], '0};
+        end
+        
+        default: begin 
+            sign_extended_immediate = '0;
+        end
+        
+        endcase
+        
+    end
+endmodule
